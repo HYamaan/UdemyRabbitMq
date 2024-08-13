c5367b5478b7447d046b6affa6d1192a387a2ff9
Hakan Yaman <hakan.yaman@d-teknoloji.com.tr>
2024-08-13 14:55:18 +0300

    Add project files.

---
+++ UdemyRabbitMq.publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://jxjiydev:GJ7PIC3hG0_JWAQ2_6oB4BQwYLM4qJVU@shrimp.rmq.cloudamqp.com/jxjiydev");
+//factory.Uri = new Uri("amqp://localhost:5672");
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+    channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+    Enumerable.Range(0, 50).ToList().ForEach(i =>
+        {
+            string message = $"{i}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "demo-queue", null, messageBody);
+            Console.WriteLine($"Mesaj:" + i );
+        }
+    );
+}
---
+++ UdemyRabbitMq.subscriper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://jxjiydev:GJ7PIC3hG0_JWAQ2_6oB4BQwYLM4qJVU@shrimp.rmq.cloudamqp.com/jxjiydev");
+
+
+    using (var connection = factory.CreateConnection())
+    using (var channel = connection.CreateModel())
+    {
+        channel.BasicQos(0, 1, false);
+
+        var consumer = new EventingBasicConsumer(channel);
+        channel.BasicConsume("demo-queue", false, consumer);
+
+        consumer.Received += (model, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine($"Received: {message}");
+                Thread.Sleep(1500);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+                
+        };
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine(); // Keep the application running
+    }
